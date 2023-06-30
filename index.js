c58ff4aa11819774882c087617257edc23ad4e4c
Shihab Hasan <shihabbd018@gmail.com>
2023-06-30 10:01:19 +0600

    inital commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+});
+
+app.get("/", (req, res)=>{
+    res.status(200).send("Server is running......")
+});
+
+app.get("/users", (req, res)=>{
+    res.status(200).send("This is the users route")
+});
